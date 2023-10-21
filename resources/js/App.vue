2cb89cb3a50c5549e389614fa94d8bc05523c7e8
abdullah <abdullah.alhumsi9@gmail.com>
2023-10-21 19:37:10 +0300

    deploy and merge front with back

---
+++ resources/js/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="wrapper">
+        <!-- sidebar -->
+        <div class="sidebar">
+            <span class="closeButton">&times;</span>
+            <p class="brand-title"><a href="">Abdo Blog</a></p>
+
+            <div class="side-links">
+                <ul>
+                    <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+                    <li><router-link :to="{ name: 'Blog' }">Blog</router-link></li>
+                    <li><router-link :to="{ name: 'About' }">About</router-link></li>
+                    <li><router-link :to="{ name: 'Contact' }">Contact</router-link></li>
+                </ul>
+            </div>
+
+            <!-- sidebar footer -->
+            <footer class="sidebar-footer">
+                <div>
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                </div>
+
+                <small>&copy 2021 Alphayo Blog</small>
+            </footer>
+        </div>
+        <!-- Menu Button -->
+        <div class="menuButton">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        <!-- main -->
+        <main class="container">
+            <!-- Render Components Depending on the page visited -->
+            <RouterView />
+        </main>
+
+        <!-- Main footer -->
+        <footer class="main-footer">
+            <div>
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+            </div>
+            <small>&copy 2021 Alphayo Blog</small>
+        </footer>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
